c1381cba894589dccdb90beb22f335cb224a800f
John Ayling <john.ayling@manifold.group>
2025-09-04 15:27:27 -0500

    Use react-native-health-link to initialize

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,42 +1,18 @@
-import AppleHealthService from "@/services/AppleHealthService";
-import GoogleHealthService from "@/services/GoogleHealthService";
-import { router, Stack } from "expo-router";
-import { useEffect, useState } from "react";
-import { ActivityIndicator, Platform, View } from "react-native";
+import { Stack } from "expo-router";
+import {
+  HealthLinkPermissions,
+  initializeHealth,
+} from "react-native-health-link";
 
-let initializeHealthData: () => Promise<boolean>;
-
-if (Platform.OS === 'ios') {
-  initializeHealthData = AppleHealthService().initializeHealthData;
-} else {
-  initializeHealthData = GoogleHealthService().initializeHealthData;
-}
+initializeHealth({
+  read: [
+    HealthLinkPermissions.Height,
+    HealthLinkPermissions.Weight,
+  ],
+  write: [],
+});
 
 export default function RootLayout() {
-  const [isInitialized, setIsInitialized] = useState(false);
-
-  useEffect(() => {
-    initializeHealthData().then((isInitialized) => {
-      setIsInitialized(isInitialized);
-    }).catch((error) => {
-      router.push('/error');
-    });
-  }, []);
-
-  if (!isInitialized) {
-    return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <ActivityIndicator size="large" color="#0000ff"></ActivityIndicator>
-      </View>
-    );
-  }
-
   return (
     <Stack>
       <Stack.Screen name="error" options={{ headerShown: false }} />
