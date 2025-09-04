5f3b5f48899278fdac52c4798f51ffcce7383b1c
John Ayling <john.ayling@manifold.group>
2025-09-04 15:57:30 -0500

    Restore loading.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,39 @@
-import { Stack } from "expo-router";
-import {
-  HealthLinkPermissions,
-  initializeHealth,
-} from "react-native-health-link";
-
-initializeHealth({
-  read: [
-    HealthLinkPermissions.Height,
-    HealthLinkPermissions.Weight,
-  ],
-  write: [],
-});
+import { router, Stack } from "expo-router";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, View } from "react-native";
+import { HealthLinkPermissions, initializeHealth } from "react-native-health-link";
 
 export default function RootLayout() {
+  const [isInitialized, setIsInitialized] = useState(false);
+
+  useEffect(() => {
+    initializeHealth({
+      read: [
+        HealthLinkPermissions.Height,
+        HealthLinkPermissions.Weight,
+      ],
+      write: [],
+    }).then(() => {
+      setIsInitialized(true);
+    }).catch((error) => {
+      router.push('/error');
+    });
+  }, []);
+
+  if (!isInitialized) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ActivityIndicator size="large" color="#0000ff"></ActivityIndicator>
+      </View>
+    );
+  }
+
   return (
     <Stack>
       <Stack.Screen name="error" options={{ headerShown: false }} />
